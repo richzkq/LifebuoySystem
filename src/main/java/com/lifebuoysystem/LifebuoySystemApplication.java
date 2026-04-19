236275b1e7097d22e4c490cc4040aafed50c61ec
zkq <comerich6@gmail.com>
2026-04-19 16:58:15 +0800

    initial code

---
+++ src/main/java/com/lifebuoysystem/LifebuoySystemApplication.java
@@ -0,0 +1,13 @@
+package com.lifebuoysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifebuoySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifebuoySystemApplication.class, args);
+    }
+
+}
