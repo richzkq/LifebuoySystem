47d138c15c5dd0fbe77b267f59d8e074563c4c6c
zkq <comerich6@gmail.com>
2026-04-20 21:24:19 +0800

    Increase CORS cross-origin resource sharing configuration

--- src/main/java/com/lifebuoysystem/config/webConfig.java
+++ src/main/java/com/lifebuoysystem/config/webConfig.java
@@ -3,6 +3,7 @@ package com.lifebuoysystem.config;
 
 import com.lifebuoysystem.interceptor.jwtInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class webConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
